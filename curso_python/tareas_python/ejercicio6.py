b12d31fea79073df91fe03c79ab05335e6bb18e6
Oscar Juela <oscar.juela@unl.edu.ec>
2022-04-05 12:48:45 -0500

    n

---
+++ curso_python/tareas_python/ejercicio6.py
@@ -0,0 +1,8 @@
+#Calcular el área y el perímetro de un Rectángulo
+
+import math
+a = float(input("ancho: "))
+b = float(input("largo: "))
+
+print("El perimietro es", a + b * 2)
+print("El área es", a * b)
