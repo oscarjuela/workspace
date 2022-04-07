d9287ded9a19c837d8bb481bb3f4f4540b3b38fd
Oscar Juela <oscar.juela@unl.edu.ec>
2022-04-07 09:07:37 -0500

    actualizacion

--- curso_python/tareas_python/tarea_unidad_1/ejercicio14.py
+++ curso_python/tareas_python/tarea_unidad_1/ejercicio14.py
@@ -5,3 +5,10 @@ por el número seguido de un signo de exclamación. Por ejemplo 5! = 1x2x3x4x5=1
 """
 
 num = int(input("Ingrese un numero: "))
+
+def factorial(num):
+    if (num == 0):
+        return 1
+    else:
+        return num * factorial(num-1)
+print("El factorial de: " + str(num) + ", es " + str(factorial(num)))
